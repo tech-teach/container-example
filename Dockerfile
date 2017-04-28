45530c324d6a47ca81b00a45421ca4b829f0319e
Oscar David Arbeláez Echeverri <odarbelaeze@gmail.com>
2017-04-28 17:41:37 -0500

    Add queue and worker

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,4 @@ FROM python:3.6
 
 ADD ./requirements.txt /
 RUN pip install -r requirements.txt
-ADD ./ /app
 WORKDIR /app
--- app.py
+++ app.py
@@ -10,5 +10,17 @@ async def ping(request):
     return text('pong')
 
 
+@app.route('/task', methods=['POST'])
+async def create_task(request):
+    # Kick of a redis queue task
+    return text('runnig', status=201)
+
+
+@app.route('/task')
+async def list_tasks(request):
+    # list the pending tasks maybe
+    return text('list of taks', status=200)
+
+
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=True)
