f38184dac0264567269831a7f94c01b4a6514ff1
Daniel Valencia <dsvalenciah@unal.edu.co>
2017-04-28 18:26:17 -0500

    add git ignore file and add rq, redis functionally

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
+import time
+
 from sanic import Sanic
 from sanic.response import text
+from rq import Queue
+from redis import Redis
 
+redis_conn = Redis(host='queue')
+q = Queue(connection=redis_conn)
 
 app = Sanic()
 
+
+def sleepear():
+    time.sleep(5)
 
 @app.route('/ping')
 async def ping(request):
@@ -13,13 +22,17 @@ async def ping(request):
 @app.route('/task', methods=['POST'])
 async def create_task(request):
     # Kick of a redis queue task
+    q.enqueue('app.sleepear')
     return text('runnig', status=201)
 
 
 @app.route('/task')
 async def list_tasks(request):
     # list the pending tasks maybe
-    return text('list of taks', status=200)
+    return text(
+        str(q.count),
+        status=200
+    )
 
 
 if __name__ == '__main__':
